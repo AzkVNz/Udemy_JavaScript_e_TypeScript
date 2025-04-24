affb1c80827afd24c3ae9a1f15cfb7ea09dd45c1
Vinícius Rosa <vnandrerosa@gmail.com>
2025-04-24 16:49:49 -0300

    Apenas subindo pasta com algumas atividades que já fiz

---
+++ JavaScript/aula20/js/js.js
@@ -0,0 +1,32 @@
+function formulario (){
+     
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    function evento (evento){
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas)
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value}` +
+        ` ${peso.value} ${altura.value}</p>`;
+    };
+
+    form.addEventListener('submit', evento);
+}
+
+formulario();
+
---
+++ JavaScript/aula8/index.js
@@ -0,0 +1,9 @@
+const nome = 'Luiz'
+const sobrenome = 'Otávio'
+const idade = 30
+const peso = 84
+const altura = 1.80
+let imc = peso / (altura * altura)
+let nascimento = 2025 - idade
+
+console.log(nome, ' tem ', idade, ' ano, pesa', peso, ' kg, tem ', altura, ' de altura, e seu IMC é de: ', imc,' . ', nome, 'nasceu em ',nascimento) 
